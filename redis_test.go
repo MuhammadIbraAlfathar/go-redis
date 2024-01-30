5150eed7c07bea0490284267cd887b24678c1a56
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-30 13:28:39 +0700

    create test string

--- redis_test.go
+++ redis_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 var client = redis.NewClient(&redis.Options{
@@ -26,4 +27,16 @@ func TestPing(t *testing.T) {
 	result, err := client.Ping(ctx).Result()
 	assert.Nil(t, err)
 	assert.Equal(t, "PONG", result)
+}
+
+func TestString(t *testing.T) {
+	client.SetEx(ctx, "name", "test", 3*time.Second)
+
+	result, err := client.Get(ctx, "name").Result()
+	assert.Nil(t, err)
+	assert.Equal(t, "test", result)
+
+	time.Sleep(5 * time.Second)
+	result, err = client.Get(ctx, "name").Result()
+	assert.NotNil(t, err)
 }
