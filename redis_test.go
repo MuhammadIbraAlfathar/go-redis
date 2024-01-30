507b6be407483ae686fff44e24ef7357b873f192
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-30 13:21:14 +0700

    create test ping

--- redis_test.go
+++ redis_test.go
@@ -1,6 +1,7 @@
 package go_redis
 
 import (
+	"context"
 	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -14,7 +15,15 @@ var client = redis.NewClient(&redis.Options{
 func TestConnection(t *testing.T) {
 	assert.NotNil(t, client)
 
-	err := client.Close()
+	//err := client.Close()
+	//
+	//assert.Nil(t, err)
+}
 
+var ctx = context.Background()
+
+func TestPing(t *testing.T) {
+	result, err := client.Ping(ctx).Result()
 	assert.Nil(t, err)
+	assert.Equal(t, "PONG", result)
 }
